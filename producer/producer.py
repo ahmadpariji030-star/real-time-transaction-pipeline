d571cd646d30efd00e45d10605d3ac217f693409
ahmadpariji030-star <ahmadpariji030@gmail.com>
2026-04-07 11:06:20 +0700

    Add producer script

--- producer/producer.py
+++ producer/producer.py
@@ -1,1 +1,19 @@
+import json
+import time
+from kafka import KafkaProducer
 
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+    api_version=(0, 10, 1),
+    request_timeout_ms=30000
+)
+
+with open("transactions_from_kaggle.jsonl") as f:
+    for line in f:
+        data = json.loads(line)
+
+        producer.send("transactions", value=data)
+        print("Sent:", data)
+
+        time.sleep(1)
